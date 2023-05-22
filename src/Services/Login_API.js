e4fa66b660471d342943c7ccee50e896c62a9d6d
Leo <leo0223perez@outlook.com>
2023-05-22 16:18:22 -0400

    Pasar data al backend

--- src/Services/Login_API.js
+++ src/Services/Login_API.js
@@ -1,8 +1,15 @@
+import axios from 'axios';
+
 export function Login_Sesion(user, password) {
   const DataUser = {
     User: user,
     Password: password,
   };
 
-  console.log(DataUser);
+  axios.post('http://localhost:3000/login', DataUser)
+  .then(({data})=>{
+    console.log(data);
+  }).catch(({err})=>{
+    console.log(err);
+  })
 }
