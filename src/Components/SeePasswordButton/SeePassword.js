3d59862c38c0e857c7d3c3df84b67b69fe89f37d
Leo <leo0223perez@outlook.com>
2023-05-23 09:07:55 -0400

    Added change className and type button

---
+++ src/Components/SeePasswordButton/SeePassword.js
@@ -0,0 +1,9 @@
+export function Seepassword (Input, PasswordBtn) {
+    if (Input.current.type !== "password") {
+        Input.current.type = "password";
+        PasswordBtn.current.className = "fa-solid fa-eye-slash text-black text-xl absolute right-2 cursor-pointer"
+    } else {
+        Input.current.type = "text";
+        PasswordBtn.current.className = "fa-solid fa-eye text-black text-xl absolute right-2 cursor-pointer"
+    }
+}
