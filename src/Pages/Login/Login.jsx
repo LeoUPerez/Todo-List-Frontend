42e9be43d16915f4b183098adc132a6881a4c309
Leo <leo0223perez@outlook.com>
2023-05-23 08:34:44 -0400

    Añadiendo nuevo componenente de visualizar contras

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -2,14 +2,13 @@ import React, { useEffect, useState } from 'react'
 import Register from '../../Components/Register/Register';
 import Card_Info from '../../Components/Card_Info/Card_Info';
 import { Login_Sesion } from '../../Services/Login_API';
+import SeePasswordButton from '../../Components/SeePasswordButton/SeePasswordButton'
 
 const Login = () => {
 
-  //! UseState
   const [OpenRegister, setOpenRegister] = useState(false);
   const [User, setUser] = useState("");
   const [Password, setPassword] = useState("");
-
 
   const Onchange = (setState) => (event) => {
     setState(event.target.value);
@@ -29,10 +28,12 @@ const Login = () => {
      <div className="w-2/5 h-3/5 rounded-xl bg-blue-400 relative flex">
         <Card_Info data="Login"/>
         <form className='w-3/4 h-full border-2 border-black flex justify-center items-center flex-col gap-4'>
-          <input type="text" onChange={Onchange(setUser)} value={User} className='w-1/2 min-h-[6%] p-2 rounded-lg'/>
-          <input type="text" onChange={Onchange(setPassword)} value={Password} className='w-1/2 min-h-[6%] p-2 rounded-lg'/>
+          <input onChange={Onchange(setUser)} type="text" value={User} className='w-1/2 min-h-[6%] p-1 rounded-lg'/>
+          <div className='w-1/2 min-h-[6%] flex gap-1 justify-center items-center relative'>
+           <input onChange={Onchange(setPassword)} type="password" value={Password} className='w-full h-full p-1 rounded-lg '/>
+           <SeePasswordButton/>
+          </div>
           <button onClick={(e)=>{e.preventDefault(); Login_Sesion(User, Password)}}>Iniciar sesion</button>
-          {/* Formulario Login */}
         </form>
         <button onClick={(e)=>{e.preventDefault(); OpenRegis()}} className='absolute right-1 bottom-1'>true</button>
      </div>                    
