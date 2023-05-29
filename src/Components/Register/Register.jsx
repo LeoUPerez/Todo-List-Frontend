d9d791032f53b7fa40d58d680b44a6691b03be73
Leo <leo0223perez@outlook.com>
2023-05-29 09:21:18 -0400

    Adding onchage data and services for API

--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -1,24 +1,36 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 import Card_Info from '../Card_Info/Card_Info';
+import SeePasswordButton from '../SeePasswordButton/SeePasswordButton';
+import { Register_Sesion } from '../../Services/Register_API';
+
 
 const Register = ({Open, Close}) => {
 
-const Register = useRef();
+const [User, setUser] = useState("");
+const [Password, setPassword] = useState("");
+const [Email, setEmail] = useState("");
 
-function ChangeStyleRegister(params) {
-    
+const Register = useRef();
+const InputPassword = useRef();
+
+const Onchange = (setState) => (event) => {
+  setState(event.target.value)
 }
 
   return (
     <>
     {
         Open && 
-        <div ref={Register} className='w-2/5 h-3/5 bg-slate-500 flex relative'>
+        <div ref={Register} className='w-2/5 h-3/5 rounded-xl bg-blue-400 flex relative'>
             <Card_Info data="Register"/>
-            <form action="" className='w-3/4 h-full bg-blue-400 flex justify-center items-center flex-col gap-4'>
-            <input type="text" />
-            <input type="text" />
-            <input type="text" />
+            <form action="" className='w-3/4 h-full flex justify-center items-center flex-col gap-4'>
+            <input type="text" onChange={Onchange(setUser)} className='w-1/2 min-h-[6%] p-1 rounded-lg' placeholder='Username'/>
+            <div className='w-1/2 min-h-[6%] flex gap-1 justify-center items-center relative'>
+             <input type="password" onChange={Onchange(setPassword)} ref={InputPassword} className='w-full h-full p-1 rounded-lg' placeholder='Contraseña'/>
+             <SeePasswordButton Input={InputPassword} />
+            </div>
+            <input type="text" onChange={Onchange(setEmail)} className='w-1/2 min-h-[6%] p-1 rounded-lg' placeholder='Correo'/>
+            <button onClick={(e)=>{e.preventDefault(); Register_Sesion(User, Password, Email)}}>Registrarse</button>
                 {/* Formulario register */}
             </form>
             <button className='absolute right-1 bottom-1' onClick={(e)=>{e.preventDefault(); Close()}}>Falso</button>
