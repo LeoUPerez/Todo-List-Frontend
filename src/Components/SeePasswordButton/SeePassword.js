6b51c2775785e6e0f0105762dc42184c831d8dbd
Leo <leo0223perez@outlook.com>
2023-05-30 09:22:47 -0400

    adding the color gray when the inpyt type is pass

--- src/Components/SeePasswordButton/SeePassword.js
+++ src/Components/SeePasswordButton/SeePassword.js
@@ -1,7 +1,7 @@
 export function Seepassword (Input, PasswordBtn) {
     if (Input.current.type !== "password") {
         Input.current.type = "password";
-        PasswordBtn.current.className = "fa-solid fa-eye-slash text-black text-xl absolute right-2 cursor-pointer"
+        PasswordBtn.current.className = "fa-solid fa-eye-slash text-gray-400 text-xl absolute right-2 cursor-pointer"
     } else {
         Input.current.type = "text";
         PasswordBtn.current.className = "fa-solid fa-eye text-black text-xl absolute right-2 cursor-pointer"
