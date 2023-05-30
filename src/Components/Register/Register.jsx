b571a162408569de0621523d5c46adf6b276304e
Leo <leo0223perez@outlook.com>
2023-05-30 09:21:24 -0400

    changing the desing of register

--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -2,6 +2,7 @@ import React, { useRef, useState } from 'react'
 import Card_Info from '../Card_Info/Card_Info';
 import SeePasswordButton from '../SeePasswordButton/SeePasswordButton';
 import { Register_Sesion } from '../../Services/Register_API';
+import './scroll.css'
 
 
 const Register = ({Open, Close}) => {
@@ -21,19 +22,20 @@ const Onchange = (setState) => (event) => {
     <>
     {
         Open && 
-        <div ref={Register} className='w-2/5 h-3/5 rounded-xl bg-blue-400 flex relative'>
-            <Card_Info data="Register"/>
-            <form action="" className='w-3/4 h-full flex justify-center items-center flex-col gap-4'>
-            <input type="text" onChange={Onchange(setUser)} className='w-1/2 min-h-[6%] p-1 rounded-lg' placeholder='Username'/>
-            <div className='w-1/2 min-h-[6%] flex gap-1 justify-center items-center relative'>
-             <input type="password" onChange={Onchange(setPassword)} ref={InputPassword} className='w-full h-full p-1 rounded-lg' placeholder='Contraseña'/>
+        <div ref={Register} id='container_register' className='w-2/5 h-3/5 rounded-xl justify-center flex z-50 absolute left-24'>
+            {/* <Card_Info data="Register"/> */}
+            <form action="" className='w-3/4 h-full relative border rounded-2xl flex justify-center items-center flex-col gap-4'>
+             <h1 className='text-center text-6xl font-serif'>Bienvenidos</h1>
+            <input type="text" onChange={Onchange(setUser)} className='w-4/5 min-h-[6%] p-1 border rounded-lg' placeholder='Username'/>
+            <div className='w-4/5 min-h-[6%] flex gap-1 justify-center items-center relative'>
+             <input type="password" onChange={Onchange(setPassword)} ref={InputPassword} className='w-full h-full p-1 border rounded-lg' placeholder='Contraseña'/>
              <SeePasswordButton Input={InputPassword} />
             </div>
-            <input type="text" onChange={Onchange(setEmail)} className='w-1/2 min-h-[6%] p-1 rounded-lg' placeholder='Correo'/>
-            <button onClick={(e)=>{e.preventDefault(); Register_Sesion(User, Password, Email)}}>Registrarse</button>
+            <input type="text" onChange={Onchange(setEmail)} className='w-4/5 min-h-[6%] p-1 border rounded-lg' placeholder='Correo'/>
+            <button className='w-1/4 h-11 rounded-xl border transition-all duration-200 hover:bg-blue-600 hover:text-white' onClick={(e)=>{e.preventDefault(); Register_Sesion(User, Password, Email)}}>Sign in</button>
                 {/* Formulario register */}
+            <button className='absolute right-1 bottom-1 p-1 underline' onClick={(e)=>{e.preventDefault(); Close()}}>Log In</button>
             </form>
-            <button className='absolute right-1 bottom-1' onClick={(e)=>{e.preventDefault(); Close()}}>Falso</button>
         </div>
     }
     </>
